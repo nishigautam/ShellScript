fc523369bb8e580ea81d43752f91289b1eb968b2
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-03-22 02:24:49 +0530

    Added: pallindromicPrime Code

--- Day6Assignmt/FunctionsProb/pallindromePrime.sh
+++ Day6Assignmt/FunctionsProb/pallindromePrime.sh
@@ -1,12 +1,53 @@
-#! /bin/bash
+5#! /bin/bash
 
-isprime () {
-	if[[ num -lt 2 || num % 2 -eq 0 ]]
-	then
-		num = 2
-	fi
-	for (( i=3 ; i*i -le num ; i += 2 ))
+echo "Enter any Number : "
+read num;
+
+if [ $num -gt 0 ]
+then
+function check_palindrome()
+{
+	rev=0;
+	temp=$1;
+
+	while [[ $temp -gt 0 ]]
 	do
-		if [[ num % i -eq 0 ]]
-		then
+		rem=$(( $temp % 10 ))
+      temp=$(( $temp / 10 ))
+      rev=`expr $(( $rev*10 + $rem ))`;
+	done
+
+	if [ $1 -eq $rev ]
+	then
+		echo " is a Palindrome";
+	else
+		echo " is not Palindrome";
+	fi
 }
+
+function check_prime()
+{
+	check=0
+	number=$1
+	for (( i=2 ; i<$number ; i++ ))
+	do
+   	if [ $(( number%i )) -eq 0 ]
+      then
+      	check=1
+      fi
+	done
+
+	if [ $check -eq 1 ]
+	then
+		a=$( check_palindrome $number )
+      echo "$num is not a Prime number but $a"
+	else
+		a=$( check_palindrome $number )
+      echo "$num is a Prime number and $a"
+	fi
+
+}
+
+result=$((check_prime $num ))
+
+echo $result
